1ebcc19d026a0f7d4a81fdd9b729847ca5a07434
android-htc-contribute <android-htc-contribute@htc.com>
2012-04-17 17:36:32 +0800

    To add a test for the Indian locales.
    Change-Id: I167b1f624f0584f3325f186f791944c87fdb78c0

--- luni/src/test/java/libcore/java/text/DecimalFormatTest.java
+++ luni/src/test/java/libcore/java/text/DecimalFormatTest.java
@@ -76,12 +76,11 @@ public class DecimalFormatTest extends junit.framework.TestCase {
         assertDecFmtWithMultiplierAndFraction("3333.33333333", -3, 4, "-10,000");
         assertDecFmtWithMultiplierAndFraction("0.00333333", 3, 4, "0.01");
 
-        // Check DecimalFormat.
-        DecimalFormat df = (DecimalFormat)NumberFormat.getInstance();
-        if (df.toPattern().equals("#,##,##0.###")) {
+        Locale loc = Locale.getDefault();
+        if (loc.getLanguage().equals("en") && loc.getCountry().equals("IN")) {
             assertDecFmtWithMultiplierAndFraction("3330000000000000000000000000000000", 3, 4,
                     "9,99,00,00,00,00,00,00,00,00,00,00,00,00,00,00,000");
-        } else if (df.toPattern().equals("#,##0.###")) {
+        } else {
             assertDecFmtWithMultiplierAndFraction("3330000000000000000000000000000000", 3, 4,
                     "9,990,000,000,000,000,000,000,000,000,000,000");
         }
@@ -89,8 +88,8 @@ public class DecimalFormatTest extends junit.framework.TestCase {
 
     public void testBigDecimalTestBigIntWithMultiplier() {
         // Big integer tests.
-        DecimalFormat df = (DecimalFormat)NumberFormat.getInstance();
-        if (df.toPattern().equals("#,##,##0.###")) {
+        Locale loc = Locale.getDefault();
+        if (loc.getLanguage().equals("en") && loc.getCountry().equals("IN")) {
             assertDecFmtWithMultiplierAndFraction("123456789012345", 10, 0,
                     "1,23,45,67,89,01,23,450");
             assertDecFmtWithMultiplierAndFraction("12345678901234567890", 10, 0,
@@ -104,7 +103,7 @@ public class DecimalFormatTest extends junit.framework.TestCase {
                     "-12,34,56,78,90,12,34,56,78,900");
             assertDecFmtWithMultiplierAndFraction("98765432109876543210987654321", -10, 0,
                     "-9,87,65,43,21,09,87,65,43,21,09,87,65,43,210");
-        } else if (df.toPattern().equals("#,##0.###")) {
+        } else {
             assertDecFmtWithMultiplierAndFraction("123456789012345", 10, 0,
                     "1,234,567,890,123,450");
             assertDecFmtWithMultiplierAndFraction("12345678901234567890", 10, 0,
