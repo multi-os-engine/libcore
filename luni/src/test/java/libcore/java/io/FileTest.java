8f3cf2fb1c4e1eeb5a573517647c297aaebaa153
Kalyani polepeddy <kpolep@codeaurora.org>
2012-04-19 11:07:07 +0530

    Libcore : Limit change for directory path length
    Modify test_longReadlink to only test up to a maximum of 159
    characters for yaffs2.
    
    Change-Id: Iab9481e23c916792826f124783cf5b1a3045219c

--- luni/src/test/java/libcore/java/io/FileTest.java
+++ luni/src/test/java/libcore/java/io/FileTest.java
@@ -42,13 +42,13 @@ public class FileTest extends junit.framework.TestCase {
     }
 
     private static File createDeepStructure(File base) throws Exception {
-        // ext has a limit of around 256 characters for each path entry.
-        // 128 characters should be safe for everything but FAT.
-        String longString = longString(128);
-        // Keep creating subdirectories until the path length is greater than 1KiB.
+        // 64 character string length should be safe for everything.
+        int strlen = 64;
+        String longString = longString(strlen);
+        // Keep creating subdirectories until the path length is max of 159 bytes.
         // Ubuntu 8.04's kernel is happy up to about 4KiB.
         File f = base;
-        for (int i = 0; f.toString().length() <= 1024; ++i) {
+        for (int i = 0;( f.toString().length() + strlen) <= 159; ++i) {
             f = new File(f, longString);
             assertTrue(f.mkdir());
         }
@@ -63,13 +63,14 @@ public class FileTest extends junit.framework.TestCase {
     }
 
     // readlink(2) is a special case,.
+    //Modify test_longReadlink to only test up to a maximum of 159 characters for yaffs2.
     public void test_longReadlink() throws Exception {
         File base = createTemporaryDirectory();
         File target = createDeepStructure(base);
         File source = new File(base, "source");
         assertFalse(source.exists());
         assertTrue(target.exists());
-        assertTrue(target.getCanonicalPath().length() > 1024);
+        assertTrue(target.getCanonicalPath().length() <= 159);
         ln_s(target, source);
         assertTrue(source.exists());
         assertEquals(target.getCanonicalPath(), source.getCanonicalPath());
