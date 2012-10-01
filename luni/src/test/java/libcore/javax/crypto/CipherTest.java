745c388b8526b44ab40be331f7dcee98a4221543
Kenny Root <kroot@google.com>
2012-10-01 10:12:15 -0700

    More testing of Cipher .doFinal variants
    Change-Id: I5f94eac56da177de5d395277f246263af32c67c3

--- luni/src/test/java/libcore/javax/crypto/CipherTest.java
+++ luni/src/test/java/libcore/javax/crypto/CipherTest.java
@@ -1720,8 +1720,34 @@ public final class CipherTest extends TestCase {
 
         c.init(Cipher.DECRYPT_MODE, key, spec);
 
-        final byte[] actualPlaintext = c.doFinal(p.ciphertext);
-        assertTrue(Arrays.equals(p.plaintext, actualPlaintext));
+        // Try the normal .doFinal(input) call
+        {
+            final byte[] actualPlaintext = c.doFinal(p.ciphertext);
+            assertTrue(Arrays.equals(p.plaintext, actualPlaintext));
+        }
+
+        // .doFinal(input, offset, len, output),
+        {
+            final byte[] largerThanCiphertext = new byte[p.ciphertext.length + 5];
+            System.arraycopy(p.ciphertext, 0, largerThanCiphertext, 5, p.ciphertext.length);
+            final byte[] actualPlaintext = new byte[p.plaintext.length];
+
+            assertEquals(p.plaintext.length,
+                    c.doFinal(largerThanCiphertext, 5, p.ciphertext.length, actualPlaintext));
+            assertTrue(Arrays.equals(p.plaintext, actualPlaintext));
+        }
+
+        // Try .doFinal(input, offset, len, output, offset)
+        {
+            final byte[] largerThanCiphertext = new byte[p.ciphertext.length + 10];
+            System.arraycopy(p.ciphertext, 0, largerThanCiphertext, 5, p.ciphertext.length);
+            final byte[] actualPlaintext = new byte[p.plaintext.length + 2];
+
+            assertEquals(p.plaintext.length,
+                    c.doFinal(largerThanCiphertext, 5, p.ciphertext.length, actualPlaintext, 1));
+            assertTrue(Arrays.equals(p.plaintext,
+                    Arrays.copyOfRange(actualPlaintext, 1, p.plaintext.length + 2)));
+        }
 
         Cipher cNoPad = Cipher.getInstance(p.mode + "/NoPadding");
         cNoPad.init(Cipher.DECRYPT_MODE, key, spec);
