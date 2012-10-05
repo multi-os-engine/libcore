c7831fe506d8c64bbc5a5efbdc422f12a252eacf
Kenny Root <kroot@google.com>
2012-10-05 12:39:48 -0700

    NativeCrypto: add test for EVP_SignInit
    EVP_SignInit was not clearing OpenSSL errors when it encountered a bad
    algorithm name.
    
    Change-Id: I1b2a18ce51c784e0e9a3fb37574b34180c6f8719

--- luni/src/test/java/org/apache/harmony/xnet/provider/jsse/NativeCryptoTest.java
+++ luni/src/test/java/org/apache/harmony/xnet/provider/jsse/NativeCryptoTest.java
@@ -1834,6 +1834,39 @@ public class NativeCryptoTest extends TestCase {
         assertNoErrors();
     }
 
+    public void test_EVP_get_digestbyname() throws Exception {
+        assertTrue(NativeCrypto.EVP_get_digestbyname("sha256") != NULL);
+
+        try {
+            NativeCrypto.EVP_get_digestbyname(null);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+
+        try {
+            NativeCrypto.EVP_get_digestbyname("");
+            NativeCrypto.EVP_get_digestbyname("foobar");
+            fail();
+        } catch (RuntimeException expected) {
+        }
+
+        assertNoErrors();
+    }
+
+    public void test_EVP_SignInit() throws Exception {
+        final int ctx = NativeCrypto.EVP_SignInit("RSA-SHA256");
+        assertTrue(ctx != NULL);
+        NativeCrypto.EVP_MD_CTX_destroy(ctx);
+
+        try {
+            NativeCrypto.EVP_SignInit("foobar");
+            fail();
+        } catch (RuntimeException expected) {
+        }
+
+        assertNoErrors();
+    }
+
     /*
      * Test vector generation:
      * openssl rand -hex 16
