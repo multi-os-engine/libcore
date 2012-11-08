77fbe0fb4306c0e0963b2720b4d08f241a5c8d3c
Elliott Hughes <enh@google.com>
2012-11-08 14:28:29 -0800

    Improve the System.currentTimeMillis/Date/DateFormat/SimpleDateFormat documentation.
    15 years later, this stuff is still causing confusion.
    
    Change-Id: Id68d43014b72de2cf620cf1798517106fd68d229

--- luni/src/main/java/java/text/SimpleDateFormat.java
+++ luni/src/main/java/java/text/SimpleDateFormat.java
@@ -138,7 +138,7 @@ import libcore.icu.TimeZones;
  * }
  * </pre>
  *
- * <p>Produces this output when run on an {@code en_US} device in the PDT time zone:
+ * <p>Produces this output when run on an {@code en_US} device in the America/Los_Angeles time zone:
  * <pre>
  * Dec 31, 1969
  * Dec 31, 1969 4:00:00 PM
@@ -163,7 +163,7 @@ import libcore.icu.TimeZones;
  * }
  * </pre>
  *
- * <p>Which produces this output when run in the PDT time zone:
+ * <p>Which produces this output when run in the America/Los_Angeles time zone:
  * <pre>
  *                     yyyy-MM-dd 1969-12-31
  *                     yyyy-MM-dd 1970-01-01
@@ -187,7 +187,9 @@ import libcore.icu.TimeZones;
  * the original {@code Date}. For this
  * reason it is almost always necessary and desirable to include the timezone in the output.
  * It may also be desirable to set the formatter's time zone to UTC (to ease comparison, or to
- * make logs more readable, for example).
+ * make logs more readable, for example). It is often best to avoid formatting completely when
+ * writing dates/times in machine-readable form. Simply sending the "Unix time" as an integer is
+ * cheaper and unambiguous, and can be formatted any way the recipient deems appropriate.
  *
  * <h4>Synchronization</h4>
  * {@code SimpleDateFormat} is not thread-safe. Users should create a separate instance for
