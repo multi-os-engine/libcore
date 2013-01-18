90caf3ee1eecc431d306bd7bd1e8bc43d415fd8b
Brian Carlstrom <bdc@google.com>
2013-01-18 12:16:08 -0800

    Make test_SSLSocket_getSupportedCipherSuites_connect continue to next provider on exception
    Change-Id: I7d124b4508ffbbde32e60d6901df5f386ed7c5d9

--- luni/src/test/java/libcore/javax/net/ssl/SSLSocketTest.java
+++ luni/src/test/java/libcore/javax/net/ssl/SSLSocketTest.java
@@ -73,41 +73,49 @@ public class SSLSocketTest extends TestCase {
                 .aliasPrefix("rsa-dsa-ec")
                 .ca(true)
                 .build();
+        StringBuilder error = new StringBuilder();
         if (StandardNames.IS_RI) {
-            test_SSLSocket_getSupportedCipherSuites_connect(testKeyStore,
-                                                            StandardNames.JSSE_PROVIDER_NAME,
-                                                            StandardNames.JSSE_PROVIDER_NAME,
-                                                            true,
-                                                            true);
-        } else  {
-            test_SSLSocket_getSupportedCipherSuites_connect(testKeyStore,
-                                                            "HarmonyJSSE",
-                                                            "HarmonyJSSE",
-                                                            false,
-                                                            false);
-            test_SSLSocket_getSupportedCipherSuites_connect(testKeyStore,
-                                                            "AndroidOpenSSL",
-                                                            "AndroidOpenSSL",
-                                                            true,
-                                                            true);
-            test_SSLSocket_getSupportedCipherSuites_connect(testKeyStore,
-                                                            "HarmonyJSSE",
-                                                            "AndroidOpenSSL",
-                                                            false,
-                                                            true);
-            test_SSLSocket_getSupportedCipherSuites_connect(testKeyStore,
-                                                            "AndroidOpenSSL",
-                                                            "HarmonyJSSE",
-                                                            true,
-                                                            false);
+            error.append(test_SSLSocket_getSupportedCipherSuites_connect(
+                testKeyStore,
+                StandardNames.JSSE_PROVIDER_NAME,
+                StandardNames.JSSE_PROVIDER_NAME,
+                true,
+                true));
+        } else {
+            error.append(test_SSLSocket_getSupportedCipherSuites_connect(testKeyStore,
+                                                                         "HarmonyJSSE",
+                                                                         "HarmonyJSSE",
+                                                                         false,
+                                                                         false));
+            error.append(test_SSLSocket_getSupportedCipherSuites_connect(testKeyStore,
+                                                                         "AndroidOpenSSL",
+                                                                         "AndroidOpenSSL",
+                                                                         true,
+                                                                         true));
+            error.append(test_SSLSocket_getSupportedCipherSuites_connect(testKeyStore,
+                                                                         "HarmonyJSSE",
+                                                                         "AndroidOpenSSL",
+                                                                         false,
+                                                                         true));
+            error.append(test_SSLSocket_getSupportedCipherSuites_connect(testKeyStore,
+                                                                         "AndroidOpenSSL",
+                                                                         "HarmonyJSSE",
+                                                                         true,
+                                                                         false));
         }
 
+        if (error.length() != 0) {
+            throw new Exception("One or more problems in "
+                                + "test_SSLSocket_getSupportedCipherSuites_connect:\n" + error);
+        }
     }
-    private void test_SSLSocket_getSupportedCipherSuites_connect(TestKeyStore testKeyStore,
-                                                                 String clientProvider,
-                                                                 String serverProvider,
-                                                                 boolean clientSecureRenegotiation,
-                                                                 boolean serverSecureRenegotiation)
+
+    private StringBuilder test_SSLSocket_getSupportedCipherSuites_connect(
+        TestKeyStore testKeyStore,
+        String clientProvider,
+        String serverProvider,
+        boolean clientSecureRenegotiation,
+        boolean serverSecureRenegotiation)
             throws Exception {
 
         String clientToServerString = "this is sent from the client to the server...";
@@ -186,6 +194,9 @@ public class SSLSocketTest extends TestCase {
                     String message = ("Problem trying to connect cipher suite " + cipherSuite
                                       + " client=" + clientProvider
                                       + " server=" + serverProvider);
+                    if (false) {
+                        throw new Exception(message);
+                    }
                     System.out.println(message);
                     maybeExpected.printStackTrace();
                     error.append(message);
@@ -193,11 +204,8 @@ public class SSLSocketTest extends TestCase {
                 }
             }
         }
-        if (error.length() != 0) {
-            throw new Exception("One or more problems in "
-                                + "test_SSLSocket_getSupportedCipherSuites_connect:\n" + error);
-        }
         c.close();
+        return error;
     }
 
     public void test_SSLSocket_getEnabledCipherSuites() throws Exception {
