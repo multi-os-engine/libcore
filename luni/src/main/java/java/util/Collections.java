65115eec80ca1ded8e5cbe2775fc06860e34fedd
Ian Rogers <irogers@google.com>
2013-02-06 10:12:43 -0800

    Secondary hash for IdentityHashMap and WeakHashMap.
    Dalvik's identity hash code is a multiple of 8 meaning that it can cause
    frequent collisions if a secondary hash isn't applied. This change adds
    a secondary hash function to IdentityHashMap and WeakHashMap, as is done
    in HashMap, HashTable and ConcurrentHashMap. There is some other minor clean-up
    including updating the secondary hash function to match that in
    ConcurrentHashMap.
    
    Change-Id: Id2115b2622ff759bf2f67bd7572c2ecd8f0f0d67

--- luni/src/main/java/java/util/Collections.java
+++ luni/src/main/java/java/util/Collections.java
@@ -3399,4 +3399,50 @@ public class Collections {
             return sm.lastKey();
         }
     }
+
+    /**
+     * Computes a hashCode and applies a supplemental hash function, which defends
+     * against poor quality hash functions. This is critical because HashMap
+     * uses power-of-two length hash tables, that otherwise encounter collisions
+     * for hashCodes that do not differ in lower or upper bits.
+     * Routine taken from java.util.concurrent.ConcurrentHashMap.hash(int).
+     */
+    static int secondaryHash(Object key) {
+	return secondaryHash(key.hashCode());
+    }
+
+    static int secondaryIdentityHash(Object key) {
+	return secondaryHash(System.identityHashCode(key));
+    }
+
+    private static int secondaryHash(int h) {
+	// Spread bits to regularize both segment and index locations,
+	// using variant of single-word Wang/Jenkins hash.
+	h += (h <<  15) ^ 0xffffcd7d;
+        h ^= (h >>> 10);
+        h += (h <<   3);
+        h ^= (h >>>  6);
+        h += (h <<   2) + (h << 14);
+        return h ^ (h >>> 16);
+    }
+
+    /**
+     * Returns the smallest power of two >= its argument, with several caveats:
+     * If the argument is negative but not Integer.MIN_VALUE, the method returns
+     * zero. If the argument is > 2^30 or equal to Integer.MIN_VALUE, the method
+     * returns Integer.MIN_VALUE. If the argument is zero, the method returns
+     * zero.
+     */
+    static int roundUpToPowerOfTwo(int i) {
+        i--; // If input is a power of two, shift its high-order bit right
+
+        // "Smear" the high-order bit all the way to the right
+        i |= i >>>  1;
+        i |= i >>>  2;
+        i |= i >>>  4;
+        i |= i >>>  8;
+        i |= i >>> 16;
+
+        return i + 1;
+    }
 }
