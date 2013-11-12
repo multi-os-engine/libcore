4133e0893a95c14b1e18f45732450b35d14223d3
Narayan Kamath <narayan@google.com>
2013-11-12 11:23:08 +0000

    Dedup tests under api with apache-harmony.
    This batch of changes deals with harmony tests that
    are already present under luni/src/test/java/tests/api.
    
    The following tests were exact copies and needed no
    additional changes.
    
    Process2Test.java,
    ProcessTest.java
    PhantomReferenceTest.java
    ReferenceQueueTest.java
    ReferenceTest.java
    SoftReferenceTest.java
    WeakReferenceTest.java
    AccessibleObjectTest.java
    ArrayTest.java
    ConstructorTest.java
    FieldTest.java
    GenericSignatureFormatErrorTest.java
    InvocationTargetExceptionTest.java
    AbstractQueueTest.java
    AbstractSequentialListTest.java
    CalendarTest.java
    ConcurrentModTest.java
    concurrentModificationExceptionTest.java
    EmptyStackExcepionTest.java
    EnumMapTest.java
    EnumSetTest.java
    FormattableFlagsTest.java
    HashSetTest.java
    MissingResourceExceptionTest.java
    NoSuchElementExceptionTest.java
    ObservableTest.java
    SampleBundleClass.java
    SimpleTimezoneTest.java
    TimerTaskTest.java
    TimerTest.java
    
    A follow up change will deal with harmony tests that aren't
    in this directory yet.
    
    Change-Id: I04b1168601152f81c12abb31d3b1156975b15187

--- luni/src/test/java/tests/api/java/lang/Process2Test.java
+++ luni/src/test/java/tests/api/java/lang/Process2Test.java
@@ -33,7 +33,6 @@ public class Process2Test extends junit.framework.TestCase {
      * java.lang.Process#getInputStream(),
      *        java.lang.Process#getErrorStream()
      *        java.lang.Process#getOutputStream()
-     * Tests if these methods return buffered streams.
      */
     @AndroidOnly("dalvikvm specific")
     public void test_streams()
@@ -42,46 +41,5 @@ public class Process2Test extends junit.framework.TestCase {
         assertNotNull(p.getInputStream());
         assertNotNull(p.getErrorStream());
         assertNotNull(p.getOutputStream());
-    }
-
-    public void test_getErrorStream() {
-        String[] commands = {"ls"};
-        Process process = null;
-        try {
-            process = Runtime.getRuntime().exec(commands, null, null);
-            InputStream is = process.getErrorStream();
-            StringBuffer msg = new StringBuffer("");
-            while (true) {
-                int c = is.read();
-                if (c == -1)
-                    break;
-                msg.append((char) c);
-            }
-            assertEquals("", msg.toString());
-        } catch (IOException e) {
-            fail("IOException was thrown.");
-        } finally {
-            process.destroy();
-        }
-
-        String[] unknownCommands = {"mkdir", "-u", "test"};
-        Process erProcess = null;
-        try {
-            erProcess = Runtime.getRuntime().exec(unknownCommands, null, null);
-            InputStream is = erProcess.getErrorStream();
-            StringBuffer msg = new StringBuffer("");
-            while (true) {
-                int c = is.read();
-                if (c == -1)
-                    break;
-                msg.append((char) c);
-            }
-            assertTrue("Error stream should not be empty",
-                                                !"".equals(msg.toString()));
-        } catch (IOException e) {
-            fail("IOException was thrown.");
-        } finally {
-            erProcess.destroy();
-        }
     }
 }
