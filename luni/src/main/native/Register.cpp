ce7102e3afb8cdac1c01a1139e17b79231eafa18
Jeff Hao <jeffhao@google.com>
2014-01-15 13:51:48 -0800

    Removed offset and value from String and added StringFactory.
    Change-Id: I55314ceb906d0bf7e78545dcd9bc3489a5baf03f

--- luni/src/main/native/Register.cpp
+++ luni/src/main/native/Register.cpp
@@ -48,7 +48,6 @@ jint JNI_OnLoad(JavaVM* vm, void*) {
     REGISTER(register_java_lang_System);
     REGISTER(register_java_math_NativeBN);
     REGISTER(register_java_nio_ByteOrder);
-    REGISTER(register_java_nio_charset_Charsets);
     REGISTER(register_java_text_Bidi);
     REGISTER(register_java_util_jar_StrictJarFile);
     REGISTER(register_java_util_regex_Matcher);
