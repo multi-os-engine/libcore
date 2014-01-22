dc91a18b80a7db59bfd8623cb2b244ac14a821ac
Narayan Kamath <narayan@google.com>
2014-01-22 12:53:31 +0000

    Fix ComputeSerialVersionUIDTest.
    The golden files were referring to the wrong class.
    
    Also, add code to generate golden files. The original files
    seem to have been generated by hand.
    
    bug: 12490445
    
    Change-Id: Ia65b6d65bc9a3d4cbc62a7a0f7bdfc004e7315e2

--- harmony-tests/src/test/java/org/apache/harmony/tests/java/io/ComputeSerialVersionUIDTest.java
+++ harmony-tests/src/test/java/org/apache/harmony/tests/java/io/ComputeSerialVersionUIDTest.java
@@ -1,6 +1,8 @@
 package org.apache.harmony.tests.java.io;
 
+import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class ComputeSerialVersionUIDTest extends junit.framework.TestCase {
 
@@ -142,4 +144,76 @@ public class ComputeSerialVersionUIDTest extends junit.framework.TestCase {
                 .readObject();
         ois.close();
     }
+
+    private void generateGoldenFiles(String path) throws Exception {
+         ObjectOutputStream out = new ObjectOutputStream(
+                 new FileOutputStream(path + "/testComputeSUIDClass.ser"));
+         out.writeObject(new SerializationTestClass.TestClassName1());
+         out.writeObject(new SerializationTestClass.TestClassName2T_T$T());
+         out.writeObject(new SerializationTestClass.TestClassModifierPublic());
+         out.writeObject(new SerializationTestClass.TestClassModifierAbstract());
+         out.writeObject(new SerializationTestClass.TestClassModifierFinal());
+         out.writeObject(new SerializationTestClass.TestClassModifierInterface());
+         out.close();
+
+         out = new ObjectOutputStream(new FileOutputStream(path + "/testComputeSUIDInterfaces.ser"));
+         out.writeObject(new SerializationTestClass.TestIntefaces());
+         out.writeObject(new SerializationTestClass.TestIntefacesA());
+         out.writeObject(new SerializationTestClass.TestIntefacesAB());
+         out.writeObject(new SerializationTestClass.TestIntefacesBA());
+         out.writeObject(new SerializationTestClass.TestIntefacesC());
+         out.writeObject(new SerializationTestClass.TestIntefacesAC());
+         out.writeObject(new SerializationTestClass.TestIntefacesCA());
+         out.writeObject(new SerializationTestClass.TestIntefacesABC());
+         out.writeObject(new SerializationTestClass.TestIntefacesACB());
+         out.writeObject(new SerializationTestClass.TestIntefacesBAC());
+         out.writeObject(new SerializationTestClass.TestIntefacesBCA());
+         out.writeObject(new SerializationTestClass.TestIntefacesCAB());
+         out.writeObject(new SerializationTestClass.TestIntefacesCBA());
+         out.close();
+
+         out = new ObjectOutputStream(new FileOutputStream(path + "/testComputeSUIDFields.ser"));
+         out.writeObject(new SerializationTestClass.TestFieldsNone());
+         out.writeObject(new SerializationTestClass.TestFieldsOneFinal());
+         out.writeObject(new SerializationTestClass.TestFieldsTwoFinal());
+         out.writeObject(new SerializationTestClass.TestFieldsOnePrivate());
+         out.writeObject(new SerializationTestClass.TestFieldsTwoPrivate());
+         out.writeObject(new SerializationTestClass.TestFieldsOneProtected());
+         out.writeObject(new SerializationTestClass.TestFieldsTwoProtected());
+         out.writeObject(new SerializationTestClass.TestFieldsOnePublic());
+         out.writeObject(new SerializationTestClass.TestFieldsTwoPublic());
+         out.writeObject(new SerializationTestClass.TestFieldsOneStatic());
+         out.writeObject(new SerializationTestClass.TestFieldsTwoStatic());
+         out.writeObject(new SerializationTestClass.TestFieldsOneTransient());
+         out.writeObject(new SerializationTestClass.TestFieldsTwoTransient());
+         out.writeObject(new SerializationTestClass.TestFieldsOneVolatile());
+         out.writeObject(new SerializationTestClass.TestFieldsTwoVolatile());
+         out.writeObject(new SerializationTestClass.TestFieldSignatures());
+         out.close();
+
+         out = new ObjectOutputStream(new FileOutputStream(path + "/testComputeSUIDConstructors.ser"));
+         out.writeObject(new SerializationTestClass.TestConstructorNone());
+         out.writeObject(new SerializationTestClass.TestConstructorOne());
+         out.writeObject(new SerializationTestClass.TestConstructorPrivate(0));
+         out.writeObject(new SerializationTestClass.TestConstructorProtected());
+         out.writeObject(new SerializationTestClass.TestConstructorPublic());
+         out.writeObject(new SerializationTestClass.TestConstructorSignature(false, (byte) 0, '0',
+                 (short) 0, 0, 0.0f, 0.0, new Object()));
+         out.writeObject(new SerializationTestClass.TestConstructorTwo('0'));
+         out.writeObject(new SerializationTestClass.TestConstructorTwoReverse((byte) 0));
+         out.close();
+
+         out = new ObjectOutputStream(new FileOutputStream(path + "/testComputeSUIDMethods.ser"));
+         out.writeObject(new SerializationTestClass.TestMehodPrivate());
+         out.writeObject(new SerializationTestClass.TestMethodAbstract());
+         out.writeObject(new SerializationTestClass.TestMethodFinal());
+         out.writeObject(new SerializationTestClass.TestMethodNative());
+         out.writeObject(new SerializationTestClass.TestMethodProtected());
+         out.writeObject(new SerializationTestClass.TestMethodPublic());
+         out.writeObject(new SerializationTestClass.TestMethodStatic());
+         out.writeObject(new SerializationTestClass.TestMethodSignature());
+         out.writeObject(new SerializationTestClass.TestMethodReturnSignature());
+         out.writeObject(new SerializationTestClass.TestMethodSynchronized());
+         out.close();
+     }
 }
