ff12a4f7c31b193319fc97fc9cb1e568d2cf0b55
Dmitriy Ivanov <dimitry@google.com>
2014-04-25 16:18:50 -0700

    Fix library search path
     android_get_LD_LIBARAY_PATH used to return system path instead of LD_LIBRARY_PATH.
     replacing this with android_get_DEFAULT_LIBRARY_PATH().
    
    Change-Id: I54a379444bf54bb92e3535cb7d8dd6e44795af65

--- luni/src/main/native/java_lang_System.cpp
+++ luni/src/main/native/java_lang_System.cpp
@@ -35,7 +35,7 @@
 #include <unistd.h>
 
 #if defined(HAVE_ANDROID_OS)
-extern "C" void android_get_LD_LIBRARY_PATH(char*, size_t);
+extern "C" void android_get_DEFAULT_LIBRARY_PATH(char*, size_t);
 #endif
 
 static void System_log(JNIEnv* env, jclass, jchar type, jstring javaMessage, jthrowable exception) {
@@ -89,7 +89,7 @@ static jobjectArray System_specialProperties(JNIEnv* env, jclass) {
     const char* library_path = getenv("LD_LIBRARY_PATH");
 #if defined(HAVE_ANDROID_OS)
     if (library_path == NULL) {
-        android_get_LD_LIBRARY_PATH(path, sizeof(path));
+        android_get_DEFAULT_LIBRARY_PATH(path, sizeof(path));
         library_path = path;
     }
 #endif
