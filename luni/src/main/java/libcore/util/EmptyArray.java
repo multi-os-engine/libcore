f5a18cd36b498450e2ce178f0e6795859e941b0a
Michael Wright <michaelwr@google.com>
2014-07-29 16:55:02 -0700

    Add FLOAT entry to EmptyArray
    Change-Id: I70a591093557ce3dd0471bcc7e888d630c80bb88

--- luni/src/main/java/libcore/util/EmptyArray.java
+++ luni/src/main/java/libcore/util/EmptyArray.java
@@ -25,6 +25,7 @@ public final class EmptyArray {
     public static final double[] DOUBLE = new double[0];
     public static final int[] INT = new int[0];
     public static final long[] LONG = new long[0];
+    public static final float[] FLOAT = new float[0];
 
     public static final Class<?>[] CLASS = new Class[0];
     public static final Object[] OBJECT = new Object[0];
