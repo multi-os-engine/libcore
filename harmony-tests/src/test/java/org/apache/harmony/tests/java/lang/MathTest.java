82794c6b92c701838e6ea0798487939eff6bf1d4
Nicolas Geoffray <ngeoffray@google.com>
2014-12-01 10:03:09 +0000

    Use Math.ulp to account for libc/x64 differences.
    Host libc (Ubuntu EGLIBC 2.19-0ubuntu6.3) requires those deltas
    for passing the cbrt tests.
    
    Change-Id: I8c3f3848c5e3fff82aaf1c39b555e0cc1b966406

--- harmony-tests/src/test/java/org/apache/harmony/tests/java/lang/MathTest.java
+++ harmony-tests/src/test/java/org/apache/harmony/tests/java/lang/MathTest.java
@@ -127,14 +127,14 @@ public class MathTest extends junit.framework.TestCase {
         assertEquals(Double.doubleToLongBits(+0.0), Double.doubleToLongBits(Math.cbrt(+0.0)));
         assertEquals(Double.doubleToLongBits(-0.0), Double.doubleToLongBits(Math.cbrt(-0.0)));
 
-        assertEquals(3.0, Math.cbrt(27.0), 0D);
+        assertEquals(3.0, Math.cbrt(27.0), Math.ulp(3.0));
         assertEquals(23.111993172558684, Math.cbrt(12345.6), Math.ulp(23.111993172558684));
         assertEquals(5.643803094122362E102, Math.cbrt(Double.MAX_VALUE), 0D);
         assertEquals(0.01, Math.cbrt(0.000001), 0D);
 
-        assertEquals(-3.0, Math.cbrt(-27.0), 0D);
+        assertEquals(-3.0, Math.cbrt(-27.0), Math.ulp(-3.0));
         assertEquals(-23.111993172558684, Math.cbrt(-12345.6), Math.ulp(-23.111993172558684));
-        assertEquals(1.7031839360032603E-108, Math.cbrt(Double.MIN_VALUE), 0D);
+        assertEquals(1.7031839360032603E-108, Math.cbrt(Double.MIN_VALUE), Math.ulp(1.7031839360032603E-108));
         assertEquals(-0.01, Math.cbrt(-0.000001), 0D);
     }
 
