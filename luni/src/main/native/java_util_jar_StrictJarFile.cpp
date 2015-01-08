73d5b79e89082e22c43efb3c4af8fcacfcd10d5f
Narayan Kamath <narayan@google.com>
2015-01-08 13:41:33 +0000

    Cache class and field lookups in StrictJarFile.
    Iterating over a list of ZipEntries might otherwise do more
    work than necessary and create unnecessary garbage. Also, this
    is the idiom for such JNI code.
    
    Change-Id: I869289a32e0822bab9ca0345cdc3fc5aaa8adc7e

--- luni/src/main/native/java_util_jar_StrictJarFile.cpp
+++ luni/src/main/native/java_util_jar_StrictJarFile.cpp
@@ -28,16 +28,17 @@
 #include "ziparchive/zip_archive.h"
 #include "cutils/log.h"
 
+// A global reference to Class<java.util.zip.ZipEntry>.
+static jclass zipEntryClass;
+// The method ID for ZipEntry.<init>(String,String,JJJIII[BJJ)
+static jmethodID zipEntryCtor;
+
 static void throwIoException(JNIEnv* env, const int32_t errorCode) {
   jniThrowException(env, "java/io/IOException", ErrorCodeString(errorCode));
 }
 
 static jobject newZipEntry(JNIEnv* env, const ZipEntry& entry, jstring entryName) {
-  ScopedLocalRef<jclass> zipEntryClass(env, env->FindClass("java/util/zip/ZipEntry"));
-  const jmethodID zipEntryCtor = env->GetMethodID(zipEntryClass.get(), "<init>",
-                                   "(Ljava/lang/String;Ljava/lang/String;JJJIII[BJJ)V");
-
-  return env->NewObject(zipEntryClass.get(),
+  return env->NewObject(zipEntryClass,
                         zipEntryCtor,
                         entryName,
                         NULL,  // comment
@@ -164,4 +165,13 @@ static JNINativeMethod gMethods[] = {
 void register_java_util_jar_StrictJarFile(JNIEnv* env) {
   jniRegisterNativeMethods(env, "java/util/jar/StrictJarFile", gMethods, NELEM(gMethods));
 
+  jclass clazz = env->FindClass("java/util/zip/ZipEntry");
+  LOG_ALWAYS_FATAL_IF(clazz == NULL, "Unable to find java/util/zip/ZipEntry");
+
+  zipEntryClass = static_cast<jclass>(env->NewGlobalRef(clazz));
+  LOG_ALWAYS_FATAL_IF(zipEntryClass == NULL, "Unable to create global reference.");
+
+  zipEntryCtor = env->GetMethodID(zipEntryClass, "<init>",
+      "(Ljava/lang/String;Ljava/lang/String;JJJIII[BJJ)V");
+  LOG_ALWAYS_FATAL_IF(zipEntryCtor == NULL, "Unable to find ZipEntry.<init>");
 }
