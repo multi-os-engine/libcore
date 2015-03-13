69b55778a9038244f4973c06cffab19dab0c17b0
Neil Fuller <nfuller@google.com>
2015-03-13 14:31:20 +0000

    WIP: Timezone data installer code
    The code here is used by the system server to install timezone data updates.
    It is separate so it can be tested.
    
    Scripts are included that build an "update bundle" (a zip file with a
    well-defined contents).
    
    ConfigBundle contains logic to extract the bundle safely.
    TzDataBundleBuilder is used in the test and tools to construct a bundle.
    
    The script in tools is a placeholder and will evolve.
    bionic/libc/tools/zoneinfo tools will likely move there so they can be
    integrated more closely.
    
    An app is included for testing updates.
    
    Bug: 10637303
    Change-Id: Id0985f8c5be2f12858ee8bf52acf52bdb2df8741

---
+++ tzdata/tools/src/main/libcore/tzdata/update/tools/CreateTzDataBundle.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package libcore.tzdata.update.tools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.util.Properties;
+import libcore.tzdata.update.ConfigBundle;
+import libcore.tzdata.update.FileUtils;
+
+/**
+ * A command-line tool for creating a TZ data update bundle.
+ */
+public class CreateTzDataBundle {
+
+    private CreateTzDataBundle() {}
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1 || args.length > 2) {
+            printUsage();
+            System.exit(1);
+        }
+        File f = new File(args[0]);
+        if (!f.exists()) {
+            printUsage();
+            System.exit(2);
+        }
+        Properties p = loadProperties(f);
+        TzDataBundleBuilder builder = new TzDataBundleBuilder()
+                .setTzDataVersion(getMandatoryProperty(p, "tzdata.version"))
+                .addBionicTzData(getMandatoryPropertyFile(p, "bionic.file"))
+                .addIcuTzData(getMandatoryPropertyFile(p, "icu.file"));
+
+        int i = 1;
+        while (true) {
+            String localFile = p.getProperty("checksum.file.local." + i);
+            if (localFile == null) {
+                break;
+            }
+            long checksum = FileUtils.calculateChecksum(new File(localFile));
+
+            String deviceFileName = p.getProperty("checksum.file.ondevice." + i);
+            if (deviceFileName == null) {
+                break;
+            }
+            builder.addChecksum(deviceFileName, checksum);
+            i++;
+        }
+        if (i == 1) {
+            System.out.println("There must be at least one checksum file");
+        }
+
+        ConfigBundle bundle = builder.build();
+        File outputFile = new File(args[1]);
+        try (OutputStream os = new FileOutputStream(outputFile)) {
+            os.write(bundle.getBundleBytes());
+        }
+        System.out.println("Wrote: " + outputFile);
+    }
+
+    private static File getMandatoryPropertyFile(Properties p, String propertyName) {
+        String fileName = getMandatoryProperty(p, propertyName);
+        File file = new File(fileName);
+        if (!file.exists()) {
+            System.out.println(
+                    "Missing file: " + file + " for property " + propertyName + " does not exist.");
+            printUsage();
+            System.exit(4);
+        }
+        return file;
+    }
+
+    private static String getMandatoryProperty(Properties p, String propertyName) {
+        String value = p.getProperty(propertyName);
+        if (value == null) {
+            System.out.println("Missing property: " + propertyName);
+            printUsage();
+            System.exit(3);
+        }
+        return value;
+    }
+
+    private static Properties loadProperties(File f) throws IOException {
+        Properties p = new Properties();
+        try (Reader reader = new InputStreamReader(new FileInputStream(f))) {
+            p.load(reader);
+        }
+        return p;
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage:");
+        System.out.println("\t" + CreateTzDataBundle.class.getName() +
+                " <tzupdate.properties file> <output file>");
+    }
+}
