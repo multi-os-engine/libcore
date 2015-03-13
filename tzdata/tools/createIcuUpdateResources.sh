fe9085adcda94e3bd5269ec20de742e7c868ae6e
Neil Fuller <nfuller@google.com>
2015-03-13 14:31:20 +0000

    WIP: Timezone data installer code
    The code here is used by the system server to install timezone data updates.
    It is separate so it can be tested.
    
    Scripts are included that build an "update bundle" (a zip file with a
    well-defined contents).
    
    ConfigBundle contains logic to extract the bundle safely.
    TzDataBundleBuilder is used in the test and tools to construct a bundle.
    
    The script in tools is a placeholder and will evolve.
    bionic/libc/tools/zoneinfo tools will likely move there so they can be
    integrated more closely.
    
    An app is included for testing updates.
    
    Bug: 10637303
    Change-Id: Id0985f8c5be2f12858ee8bf52acf52bdb2df8741

---
+++ tzdata/tools/createIcuUpdateResources.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+
+START_DIR=`pwd`
+ICU_DIR=../../../../external/icu/icu4c/source/
+TZ_DATA_FILE=~/Downloads/tzdata2014c.tar.gz
+BUILD_DIR=${START_DIR}/icu_build
+
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+set -e
+
+${ICU_DIR}/runConfigureICU Linux
+mkdir -p ${BUILD_DIR}/bin
+cd ${BUILD_DIR}/tools/tzcode
+ln -s ${ICU_DIR}/tools/tzcode/icuregions ./icuregions
+ln -s ${ICU_DIR}/tools/tzcode/icuzones ./icuzones
+cp ${TZ_DATA_FILE} .
+make
+
+# Then make the whole thing?
+cd ${BUILD_DIR}
+make -j32
+
+ICU_LIB_DIR=${BUILD_DIR}/lib
+BIN_DIR=${BUILD_DIR}/bin
+TZ_FILES=tzdata.lst
+
+echo metaZones.res > ${TZ_FILES}
+echo timezoneTypes.res >> ${TZ_FILES}
+echo windowsZones.res >> ${TZ_FILES}
+echo zoneinfo64.res >> ${TZ_FILES}
+
+# Copy all the .res files we need here a from ./data/out/build/icudt54l
+RES_DIR=data/out/build/icudt54l
+cp ${RES_DIR}/metaZones.res ${BUILD_DIR} 
+cp ${RES_DIR}/timezoneTypes.res ${BUILD_DIR} 
+cp ${RES_DIR}/windowsZones.res ${BUILD_DIR} 
+cp ${RES_DIR}/zoneinfo64.res ${BUILD_DIR} 
+
+LD_LIBRARY_PATH=${ICU_LIB_DIR} ${BIN_DIR}/pkgdata -F -m common -v -T . -d . -p icu_tzdata ${TZ_FILES}
+cp icu_tzdata.dat ${START_DIR}
