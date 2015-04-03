fd6d3cbb3e1e56b9f82b2ed5f76c99a1479970dc
Narayan Kamath <narayan@google.com>
2015-04-03 15:51:05 +0100

    GZIPInputStream: Correctly handle extras in gzip headers.
    We were peeking into the wrong field for the extra length.
    
    bug: https://code.google.com/p/android/issues/detail?id=159340
    Change-Id: Ib7ed400e3e119b1d6c95be49d25812c5c7f366b9

--- luni/src/test/java/libcore/java/util/zip/GZIPInputStreamTest.java
+++ luni/src/test/java/libcore/java/util/zip/GZIPInputStreamTest.java
@@ -48,6 +48,17 @@ public final class GZIPInputStreamTest extends TestCase {
     private static final byte[] HELLO_WORLD_GZIPPED_WITH_HEADER_CRC = new byte[] {
         31, -117, 8, 2, 0, 0, 0, 0, 0, 0,  // 10 byte header
         29, 38, // 2 byte CRC.
+    };
+
+    /*(
+     * This is the same as {@code HELLO_WORLD_GZIPPED} except that the 4th header byte is 4
+     * (FEXTRA flag) and that the 8 bytes after the header make up the extra.
+     *
+     * Constructed manually because none of the commonly used tools appear to emit header CRCs.
+     */
+    private static final byte[] HELLO_WORLD_GZIPPED_WITH_EXTRA = new byte[] {
+        31, -117, 8, 4, 0, 0, 0, 0, 0, 0,  // 10 byte header
+        6, 0, 4, 2, 4, 2, 4, 2,  // 2 byte extra length + 6 byte extra.
         -13, 72, -51, -55, -55, 87, 8, -49, 47, -54, 73, 1, 0, 86, -79, 23, 74, 11, 0, 0, 0  // data
     };
 
@@ -57,6 +68,10 @@ public final class GZIPInputStreamTest extends TestCase {
 
     public void testShortMessageWithCrc() throws IOException {
         assertEquals("Hello World", new String(gunzip(HELLO_WORLD_GZIPPED_WITH_HEADER_CRC), "UTF-8"));
+    }
+
+    public void testShortMessageWithHeaderExtra() throws IOException {
+        assertEquals("Hello World", new String(gunzip(HELLO_WORLD_GZIPPED_WITH_EXTRA), "UTF-8"));
     }
 
     public void testLongMessage() throws IOException {
