6525f84b9b0f5058dd48bfa36a61f12f505154ca
Narayan Kamath <narayan@google.com>
2015-04-09 11:41:58 +0100

    Remove obsolete concurrent access test.
    This failure mode we're testing for is no longer possible because
    we use ICU4J. Moreover, this seems to trip up the CTS test runner
    which flags exceptions in the logs from other threads as failures.
    
    If there's a good reason to keep this, we can always add an
    UncaughtExceptionHandler that doesn't log to all worker threads.
    
    Change-Id: I23eeeb01e4a9c0987e30e54a2d84cc5d4f79a6c0

--- luni/src/test/java/libcore/java/text/BreakIteratorTest.java
+++ luni/src/test/java/libcore/java/text/BreakIteratorTest.java
@@ -53,7 +53,7 @@ public class BreakIteratorTest extends junit.framework.TestCase {
 
     // http://b/7307154 - we used to pin an unbounded number of char[]s, relying on finalization.
     public void testStress() throws Exception {
-        char[] cs = { 'a' };
+        char[] cs = {'a'};
         for (int i = 0; i < 4096; ++i) {
             BreakIterator it = BreakIterator.getWordInstance(Locale.US);
             it.setText(new String(cs));
@@ -170,34 +170,6 @@ public class BreakIteratorTest extends junit.framework.TestCase {
             fail();
         } catch (IllegalArgumentException expected) {
             // Expected exception
-        }
-    }
-
-    // http://code.google.com/p/android/issues/detail?id=41143
-    // This code is inherently unsafe and crazy;
-    // we're just trying to provoke native crashes!
-    public void testConcurrentBreakIteratorAccess() throws Exception {
-        final BreakIterator it = BreakIterator.getCharacterInstance();
-
-        ArrayList<Thread> threads = new ArrayList<Thread>();
-        for (int i = 0; i < 10; ++i) {
-            Thread t = new Thread(new Runnable() {
-                public void run() {
-                    for (int i = 0; i < 4096; ++i) {
-                        it.setText("some example text");
-                        for (int index = it.first(); index != BreakIterator.DONE; index = it.next()) {
-                        }
-                    }
-                }
-            });
-            threads.add(t);
-        }
-
-        for (Thread t : threads) {
-            t.start();
-        }
-        for (Thread t : threads) {
-            t.join();
         }
     }
 }
