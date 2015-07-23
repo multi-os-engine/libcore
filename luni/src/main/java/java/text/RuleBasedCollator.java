e11285790303520c296995f767066ce279087a0b
Rayhaan Jaufeerally <rayhaan@google.com>
2015-07-23 10:15:03 +0100

    Migrate Collator classes to use ICU4J.
    The following benchmarks have been run and show the performance
    gains from calling ICU4J.
    
    ----- ICU4J -----
                benchmark   us linear runtime
        CollatorIdentical 6.90 ==============================
          CollatorPrimary 3.20 =============
        CollatorSecondary 3.79 ================
         CollatorTertiary 4.28 ==================
        vm: app_process
    
    ----- ICU4C -----
                benchmark    us linear runtime
        CollatorIdentical 11.75 ==============================
          CollatorPrimary 10.01 =========================
        CollatorSecondary  9.88 =========================
         CollatorTertiary 10.26 ==========================
        vm: app_process
    
    All tests relating to Collation pass after this change.
    
    With respect to CJK character loading time, tests were done on a
    Nexus 6 running this change, in the US locale and TRADITIONAL_CHINESE
    locale.
    
    In EN-US:
    	Time to load Traditional Chinese collator after boot: 76ms, 76ms
    	Time to load again: (39ms, 36ms, 36ms), (36ms, 36ms, 48ms)
    In ZH-TW:
    	Time to load Traditional Chinese collator after boot: 37ms, 36ms
    	Time to load again: (36ms, 36ms, 36ms), (35ms, 37ms, 35ms)
    
    This seems to indicate the system is creating a Collator on boot meaning
    that ICU is caching it after the first creation. So if a device is in the
    Traditional Chinese locale, the cost of creating Traditional Chinese
    collators should have been absorbed at boot time.
    
    Change-Id: I98a1c32c8f58e03d16d694b8a4fca4ced961af8e

--- luni/src/main/java/java/text/RuleBasedCollator.java
+++ luni/src/main/java/java/text/RuleBasedCollator.java
@@ -17,7 +17,7 @@
 
 package java.text;
 
-import libcore.icu.RuleBasedCollatorICU;
+import libcore.icu.CollationKeyICU;
 
 /**
  * A concrete subclass of {@link Collator}.
@@ -76,8 +76,13 @@ import libcore.icu.RuleBasedCollatorICU;
  * {@code ParseException}.
  */
 public class RuleBasedCollator extends Collator {
-    RuleBasedCollator(RuleBasedCollatorICU wrapper) {
+
+    RuleBasedCollator(com.ibm.icu.text.RuleBasedCollator wrapper) {
         super(wrapper);
+    }
+
+    com.ibm.icu.text.RuleBasedCollator collAsICU() {
+        return (com.ibm.icu.text.RuleBasedCollator) icuColl;
     }
 
     /**
@@ -98,11 +103,12 @@ public class RuleBasedCollator extends Collator {
      *             syntax.
      */
     public RuleBasedCollator(String rules) throws ParseException {
+
         if (rules == null) {
             throw new NullPointerException("rules == null");
         }
         try {
-            icuColl = new RuleBasedCollatorICU(rules);
+            icuColl = new com.ibm.icu.text.RuleBasedCollator(rules);
         } catch (Exception e) {
             if (e instanceof ParseException) {
                 throw (ParseException) e;
@@ -111,7 +117,9 @@ public class RuleBasedCollator extends Collator {
              * -1 means it's not a ParseException. Maybe IOException thrown when
              * an error occurred while reading internal data.
              */
-            throw new ParseException(e.getMessage(), -1);
+            ParseException pe = new ParseException(e.getMessage(), -1);
+            pe.initCause(e);
+            throw pe;
         }
     }
 
@@ -128,7 +136,8 @@ public class RuleBasedCollator extends Collator {
         if (source == null) {
             throw new NullPointerException("source == null");
         }
-        return new CollationElementIterator(icuColl.getCollationElementIterator(source));
+        return new CollationElementIterator(
+                ((com.ibm.icu.text.RuleBasedCollator) icuColl).getCollationElementIterator(source));
     }
 
     /**
@@ -142,7 +151,7 @@ public class RuleBasedCollator extends Collator {
         if (source == null) {
             throw new NullPointerException("source == null");
         }
-        return new CollationElementIterator(icuColl.getCollationElementIterator(source));
+        return new CollationElementIterator(collAsICU().getCollationElementIterator(source));
     }
 
     /**
@@ -153,7 +162,7 @@ public class RuleBasedCollator extends Collator {
      * The string forms of the collation rules are omitted to save space on the device.
      */
     public String getRules() {
-        return icuColl.getRules();
+        return collAsICU().getRules();
     }
 
     /**
@@ -202,12 +211,16 @@ public class RuleBasedCollator extends Collator {
      */
     @Override
     public CollationKey getCollationKey(String source) {
-        return icuColl.getCollationKey(source);
+        if (source == null) {
+            return null;
+        }
+        // TODO: Wrap com.ibm.icu.text.CollationKey directly.
+        return new CollationKeyICU(source, icuColl.getCollationKey(source).toByteArray());
     }
 
     @Override
     public int hashCode() {
-        return icuColl.getRules().hashCode();
+        return icuColl.hashCode();
     }
 
     /**
