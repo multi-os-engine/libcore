7121ae013366a8db383d24215f08b8c4a3a576a1
Rayhaan Jaufeerally <rayhaan@google.com>
2015-09-11 11:34:34 +0100

    Add benchmark for case conversion.
    The base benchmarks using ICU4C are as follows.
    
              _toLowerCaseAsciiUs 11.28 ====================
              _toUpperCaseAsciiUS 11.15 ====================
           _toUpperCaseLat1SuplFr 12.27 ======================
            _toUpperCaseLatExtACz 13.92 =========================
        _toUpperCaseLatExtBPinyin 16.35 ==============================
    
    Temporarily changing ICU.toUpperCase to call
    icu.lang.UCharacter.toUpperCase
    
              _toLowerCaseAsciiUs  11.19 ===
              _toUpperCaseAsciiUS  17.33 ====
           _toUpperCaseLat1SuplFr  40.18 ===========
            _toUpperCaseLatExtACz  69.09 ===================
        _toUpperCaseLatExtBPinyin 108.47 ==============================
    
    Change-Id: I58c2404d03e539c4c8732bd5238022ec66712a31

--- benchmarks/src/benchmarks/regression/IcuBenchmark.java
+++ benchmarks/src/benchmarks/regression/IcuBenchmark.java
@@ -19,14 +19,104 @@ package benchmarks.regression;
 import com.google.caliper.SimpleBenchmark;
 
 import java.util.Locale;
-import java.util.TimeZone;
 
 import libcore.icu.ICU;
 
 public class IcuBenchmark extends SimpleBenchmark {
-  public void time_getBestDateTimePattern(int reps) throws Exception {
-    for (int rep = 0; rep < reps; ++rep) {
-      ICU.getBestDateTimePattern("dEEEMMM", new Locale("en", "US"));
+
+    private static final String ASCII_LOWERCASE;
+    private static final String ASCII_UPPERCASE;
+
+    private static final String LAT_1_SUPPLEMENT;  // U+00C0 - U+00FF
+    private static final String LAT_EXTENDED_A;  // U+0100 - U+017F
+    private static final String LAT_EXTENDED_B;  // U+0180 - U+024F
+
+    private static final Locale CZECH_LOCALE = Locale.forLanguageTag("cs-CZ");
+    private static final Locale PINYIN_LOCALE = Locale.forLanguageTag("zh-Latn");
+
+    static {
+        char[] tmp = new char[26];
+
+        for (int i = 97; i < 123; i++) {
+            tmp[i - 97] = (char) i;
+        }
+
+        ASCII_LOWERCASE = new String(tmp);
+
+        for (int i = 65; i < 91; i++) {
+            tmp[i - 65] = (char) i;
+        }
+
+        ASCII_UPPERCASE = new String(tmp);
+
+        // Latin 1 Supplement (character part) has char codes U+00C0 - U+00FF
+        tmp = new char[0x3F];
+
+        for (int i = 0xC0; i < 0xFF; i++) {
+            tmp[i - 0xC0] = (char) i;
+        }
+
+        LAT_1_SUPPLEMENT = new String(tmp);
+
+        // Latin Extended A has char codes U+0100 - U+017F
+        tmp = new char[0x80];
+
+        for (int i = 0x100; i < 0x180; i++) {
+            tmp[i - 0x100] = (char) i;
+        }
+
+        LAT_EXTENDED_A = new String(tmp);
+
+        // Latin Extended B has char codes U+0180 - U+024F
+        tmp = new char[0xCF];
+
+        for (int i = 0x180; i < 0x24F; i++) {
+            tmp[i - 0x180] = (char) i;
+        }
+
+        LAT_EXTENDED_B = new String(tmp);
     }
-  }
+
+
+    public void time_getBestDateTimePattern(int reps) throws Exception {
+        for (int rep = 0; rep < reps; ++rep) {
+            ICU.getBestDateTimePattern("dEEEMMM", new Locale("en", "US"));
+        }
+    }
+
+    // Convert standard lowercase ASCII characters to uppercase using ICU4C in the US locale.
+    public void time_toUpperCaseAsciiUS(int reps) {
+        for (int i = 0; i < reps; i++) {
+            ICU.toUpperCase(ASCII_LOWERCASE, Locale.US);
+        }
+    }
+
+    // Convert standard uppercase ASCII characters to lowercase.
+    public void time_toLowerCaseAsciiUs(int reps) {
+        for (int i = 0; i < reps; i++) {
+            ICU.toLowerCase(ASCII_UPPERCASE, Locale.US);
+        }
+    }
+
+    // Convert Latin 1 supplement characters to uppercase in France locale.
+    public void time_toUpperCaseLat1SuplFr(int reps) {
+        for (int i = 0; i < reps; i++) {
+            ICU.toUpperCase(LAT_1_SUPPLEMENT, Locale.FRANCE);
+        }
+    }
+
+    // Convert Latin Extension A characters to uppercase in Czech locale
+    public void time_toUpperCaseLatExtACz(int reps) {
+        for (int i = 0; i < reps; i++) {
+            ICU.toUpperCase(LAT_EXTENDED_A, CZECH_LOCALE);
+        }
+    }
+
+    // Convert Latin Extension B characters to uppercase in Pinyin locale.
+    public void time_toUpperCaseLatExtBPinyin(int reps) {
+        for (int i = 0; i < reps; i++) {
+            ICU.toUpperCase(LAT_EXTENDED_B, PINYIN_LOCALE);
+        }
+    }
+
 }
