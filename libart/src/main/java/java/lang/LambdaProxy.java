13aa981c15f58f4511f43b07b3e7d47a5e78d5a1
Igor Murashkin <iam@google.com>
2015-10-26 18:50:11 -0700

    WIP: add lambda proxy support
    Change-Id: Ic8f1bb66ebeaed4097e758a50becf1cff6ccaefb

---
+++ libart/src/main/java/java/lang/LambdaProxy.java
@@ -0,0 +1,42 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package java.lang;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import libcore.util.EmptyArray;
+
+/**
+ * TODO.
+ * @hide
+ */
+public class LambdaProxy {
+    // TODO: This should be a type-erased "reference" to an art::lambda::Closure.
+    // TODO: only 32-bit on 32-bit systems.
+    private long closure;  // native pointer to art::lambda::Closure
+
+    @SuppressWarnings("unused")
+    protected LambdaProxy() {
+    }
+}
