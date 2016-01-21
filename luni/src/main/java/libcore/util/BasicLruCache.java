f39d9265cb33c2d514555b9071e010454121f1dc
Andreas Gampe <agampe@google.com>
2016-01-21 13:55:55 +0000

    Enum: Fix potential deadlocks caused by BasicLruCache usage.
    Don't hold a lock while initializing classes.
    
    bug: 26552570
    Change-Id: Id3ca3446af4ffc01044e213b77e13b3713025feb

--- luni/src/main/java/libcore/util/BasicLruCache.java
+++ luni/src/main/java/libcore/util/BasicLruCache.java
@@ -41,22 +41,29 @@ public class BasicLruCache<K, V> {
      * head of the queue. This returns null if a value is not cached and cannot
      * be created.
      */
-    public synchronized final V get(K key) {
+    public final V get(K key) {
         if (key == null) {
             throw new NullPointerException("key == null");
         }
 
-        V result = map.get(key);
-        if (result != null) {
-            return result;
+        V result;
+        synchronized (this) {
+            result = map.get(key);
+            if (result != null) {
+                return result;
+            }
         }
 
+        // Don't hold any locks while calling create.
         result = create(key);
 
-        if (result != null) {
-            map.put(key, result);
-            trimToSize(maxSize);
+        synchronized (this) {
+            if (result != null) {
+                map.put(key, result);
+                trimToSize(maxSize);
+            }
         }
+
         return result;
     }
 
