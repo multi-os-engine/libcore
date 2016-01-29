4845a619161afb01754036befd9c87cb72366aad
Przemyslaw Szczepaniak <pszczepaniak@google.com>
2016-01-29 12:03:31 +0000

    Android-ify SocketOptions.IP_MULTICAST_IF2
    SocketOptions#setOption(SocketOptions.IP_MULTICAST_IF2, value) on openJdk
    expects NetworkInterface object. Pre-openJdk android expects network interface
    index as an Integer. This change makes openJdk code work with both NetworkInterface object and the Integer containing network interface index.
    
    Bug: 26790580
    Change-Id: I2a5c66124aef7a816f0db59bd32cd14c48d510e8

--- ojluni/src/main/java/java/net/AbstractPlainDatagramSocketImpl.java
+++ ojluni/src/main/java/java/net/AbstractPlainDatagramSocketImpl.java
@@ -304,8 +304,11 @@ abstract class AbstractPlainDatagramSocketImpl extends DatagramSocketImpl
                  throw new SocketException("bad argument for IP_MULTICAST_IF");
              break;
          case IP_MULTICAST_IF2:
-             if (o == null || !(o instanceof NetworkInterface))
+             if (o == null || !(o instanceof Integer || o instanceof NetworkInterface))
                  throw new SocketException("bad argument for IP_MULTICAST_IF2");
+             if (o instanceof NetworkInterface) {
+                 o = new Integer(((NetworkInterface)o).getIndex());
+             }
              break;
          case IP_MULTICAST_LOOP:
              if (o == null || !(o instanceof Boolean))
