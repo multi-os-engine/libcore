04f5754b409dc4d789e74e1b58b7f065b49654dc
Shubham Ajmera <shubhamajmera@google.com>
2016-02-09 14:02:43 +0000

    Fix libcore.java.sql.OldPreparedStatementTest#testExecuteQuery
    and libcore.java.sql.OldPreparedStatementTest#testExecuteUpdate
    
    With the updgrade of SQLite to 3.9.2, A view may now reference
    undefined tables and functions when initially created. Missing
    tables and functions are reported when the VIEW is used in a
    query. The tests are modify as to accomodate the change in the
    behavior of SQLite.
    
    Bug: 26714895
    Change-Id: I116d601f2a7d97058a8d265b8b21fcd60a0a6a29

--- luni/src/test/java/libcore/java/sql/OldPreparedStatementTest.java
+++ luni/src/test/java/libcore/java/sql/OldPreparedStatementTest.java
@@ -272,8 +272,7 @@ public final class OldPreparedStatementTest extends OldSQLTest {
         String[] queries2 = {
                 "update zoo set name='Masha', family='cat' where id=;",
                 "insert into hutch (id, animal_id, address) values (1, ?,'Birds-house, 1');",
-                "insert into hutch (id, animal_id, address) values (?, 1, 'Horse-house, 5');",
-                "create view address as select address from hutch where animal_id=?"};
+                "insert into hutch (id, animal_id, address) values (?, 1, 'Horse-house, 5');"};
 
         for (int i = 0; i < queries2.length; i++) {
             PreparedStatement ps = null;
@@ -289,6 +288,21 @@ public final class OldPreparedStatementTest extends OldSQLTest {
                 } catch (Exception ee) {
                 }
             }
+        }
+
+        // With the upgrade of SQLite to 3.9.2, A view may now reference
+        // undefined tables and functions when initially created. Missing
+        // tables and functions are reported when the VIEW is used in a query.
+        try {
+            PreparedStatement ps = null;
+            String query = "create view address as select address from hutch where animal_id=?";
+            ps = conn.prepareStatement(query);
+            ps.executeQuery();
+            ps = conn.prepareStatement("select * from address");
+            ps.executeQuery();
+            fail("SQLException is not thrown for query: " + query);
+        } catch (Exception sqle) {
+            // expected
         }
 
         String query = "select * from zoo where id = ?";
@@ -324,41 +338,55 @@ public final class OldPreparedStatementTest extends OldSQLTest {
 
     // TODO Crashes VM. Fix later.
     public void testExecuteUpdate() throws SQLException {
-          String[] queries1 = { "insert into hutch (id, animal_id, address) values (1, ?, 'Birds-house, 1');",
-                              "insert into hutch (id, animal_id, address) values (?, 1, 'Horse-house, 5');",
-                              "create view address as select address from hutch where animal_id=2" };
+        String[] queries1 = { "insert into hutch (id, animal_id, address) values (1, ?, 'Birds-house, 1');",
+                "insert into hutch (id, animal_id, address) values (?, 1, 'Horse-house, 5');"};
 
-          for (int i = 0; i < queries1.length; i++) {
-              PreparedStatement ps = null;
-              try {
-                  ps = conn.prepareStatement(queries1[i]);
-                  ps.executeUpdate();
-                  fail("SQLException is not thrown for query: " + queries1[i]);
-          } catch(SQLException sqle) {
-              // expected
-          } finally {
-              try {
-                  ps.close();
-              } catch(Exception ee) {}
-          }
-      }
+        for (int i = 0; i < queries1.length; i++) {
+            PreparedStatement ps = null;
+            try {
+                ps = conn.prepareStatement(queries1[i]);
+                ps.executeUpdate();
+                fail("SQLException is not thrown for query: " + queries1[i]);
+            } catch(SQLException sqle) {
+                // expected
+            } finally {
+                try {
+                    ps.close();
+                } catch(Exception ee) {}
+            }
+        }
 
-          String query = "update zoo set name='Masha', family='cat' where id=?;";
-          PreparedStatement ps = null;
-          try {
-              ps = conn.prepareStatement(query);
-              ps.setInt(1, 2);
-              int updateCount = ps.executeUpdate();
-              assertEquals(1, updateCount);
-              ps.setInt(1, 1);
-              int updateCount1 = ps.executeUpdate();
-              assertEquals(1, updateCount1);
-          } finally {
-              try {
-                  ps.close();
-              } catch(Exception ee) {}
-          }
-      }
+        // With the upgrade of SQLite to 3.9.2, A view may now reference
+        // undefined tables and functions when initially created. Missing
+        // tables and functions are reported when the VIEW is used in a query.
+        try {
+            PreparedStatement ps = null;
+            String query = "create view address as select address from hutch where animal_id=2";
+            ps = conn.prepareStatement(query);
+            ps.executeQuery();
+            ps = conn.prepareStatement("select * from address");
+            ps.executeQuery();
+            fail("SQLException is not thrown for query: " + query);
+        } catch (Exception sqle) {
+            // expected
+        }
+
+        String query = "update zoo set name='Masha', family='cat' where id=?;";
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, 2);
+            int updateCount = ps.executeUpdate();
+            assertEquals(1, updateCount);
+            ps.setInt(1, 1);
+            int updateCount1 = ps.executeUpdate();
+            assertEquals(1, updateCount1);
+        } finally {
+            try {
+                ps.close();
+            } catch(Exception ee) {}
+        }
+    }
 
     /**
      * TODO Doesn't pass. according to spec, it is possible to invoke the
