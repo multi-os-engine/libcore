32d87feadce30a39e27ad60d5fbecccf5d210a09
Narayan Kamath <narayan@google.com>
2016-02-15 14:37:04 +0000

    SimpleDateFormatTest: Fix a couple of bogus tests involving non-olson tzs.
    The string 0001 should be treated as a minute offset, not an hour.
    
    bug: 27158550
    Change-Id: If151fc0e3a6a17e3bf4f14223d39c379d98ad4c9

--- harmony-tests/src/test/java/org/apache/harmony/tests/java/text/SimpleDateFormatTest.java
+++ harmony-tests/src/test/java/org/apache/harmony/tests/java/text/SimpleDateFormatTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.harmony.tests.java.text;
 
+import sun.util.calendar.Gregorian;
+
 import java.text.DateFormat;
 import java.text.DateFormatSymbols;
 import java.text.FieldPosition;
@@ -647,13 +649,13 @@ public class SimpleDateFormatTest extends junit.framework.TestCase {
     }
 
     public void test_parse_h_z_4DigitOffsetFromGMT() throws Exception {
-        assertParse("h z", "14 GMT-0001 ", new Date(54000000), 0, 11);
-        assertParse("h z", "14 GMT+0001 ", new Date(46800000), 0, 11);
+        assertParse("h z", "14 GMT-0100 ", new Date(54000000), 0, 11);
+        assertParse("h z", "14 GMT+0100 ", new Date(46800000), 0, 11);
     }
 
     public void test_parse_h_z_4DigitOffsetNoGMT() throws Exception {
-        assertParse("h z", "14 +0001 ", new Date(46800000), 0, 8);
-        assertParse("h z", "14 -0001 ", new Date(54000000), 0, 8);
+        assertParse("h z", "14 +0100 ", new Date(46800000), 0, 8);
+        assertParse("h z", "14 -0100 ", new Date(54000000), 0, 8);
     }
 
     public void test_parse_yyyyMMddHHmmss() throws Exception {
