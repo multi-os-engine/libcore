f27562f38f0fb6d6ce9f58d6bfd709a31e979a9b
Przemyslaw Szczepaniak <pszczepaniak@google.com>
2016-02-29 10:17:29 +0000

    Fix #close() for FileChannel created from FileDescriptor.
    Android io streams/channels created from a FileDescriptor doesn't
    own it. OpenJdk operated under different assumption. FileChannel#close()
    tries to do a preClose (dup2 using unusable file descripto) on
    FileDescriptor regardless of it's origin. preClose seems to exists
    to prevent the use of FD in case where FD value is recycled right after
    use. This was never an issue on android, and was removed from some openJdk
    classes.
    
    Bug: 27351214
    Change-Id: Ibacb192abc37870c74a2500d65b94d68f9c2318e

--- luni/src/test/java/libcore/java/nio/channels/FileChannelTest.java
+++ luni/src/test/java/libcore/java/nio/channels/FileChannelTest.java
@@ -215,6 +215,28 @@ public class FileChannelTest extends junit.framework.TestCase {
         fc.close();
     }
 
+    // b/27351214
+    public void test_close_fromFileDescriptor() throws Exception {
+        // Create a valid FileDescriptor
+        File tmp = File.createTempFile("FileChannelTest", "tmp");
+        FileOutputStream fos = new FileOutputStream(tmp);
+        FileDescriptor fd = fos.getFD();
+        assertTrue(fd.valid());
+
+        // Create FileOutputStream from FileDescriptor
+        FileOutputStream fosFromFd = new FileOutputStream(fd);
+        // Create FileChannel from FileOutputStream created from FileDescriptor
+        FileChannel fc = fosFromFd.getChannel();
+
+        // Invalidate FileDescriptor
+        fos.close();
+        assertFalse(fd.valid());
+
+        // Close FileOutputStream and therefore close the FileChannel.
+        fosFromFd.close();
+    }
+
+
     private static FileChannel createFileContainingBytes(byte[] bytes) throws IOException {
         File tmp = File.createTempFile("FileChannelTest", "tmp");
         FileOutputStream fos = new FileOutputStream(tmp, true);
