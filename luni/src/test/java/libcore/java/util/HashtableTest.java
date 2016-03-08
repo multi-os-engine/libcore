0afab9f49f7dd49d5fc7a0b7d59c8993c7e10cbc
Shubham Ajmera <shubhamajmera@google.com>
2016-03-08 16:10:19 +0000

    Port OpenJDK8 changes in java.util.Map
    & add tests.
    
    The CL add tests for map default methods added in Java 8 - getOrDefault,
    putIfAbsent, remove(K, V), replace(K, V, V), replace(K, V), forEach,
    replaceAll, compute, computeIfAbsent, computeIfPresent, putIfAbsent,
    merge.
    
    Methods have been made synchronzed in Hashtable. They are using the
    default map methods instead of taking the upstream changes.
    
    Replace(K, V, V) in LinkedHashMap has been  overridden as it doesn't
    record access when the replace method doesn't make any replacement.
    
    Bug: 27426743
    
    Change-Id: Ifb2130698dd0ed42acc5c0f429496eebb444e21c

---
+++ luni/src/test/java/libcore/java/util/HashtableTest.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package libcore.java.util;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class HashtableTest extends junit.framework.TestCase {
+
+    public void test_getOrDefault() {
+        MapDefaultMethodTester.test_getOrDefault(new Hashtable<>(), false/*DoesNotAcceptNullKey*/);
+
+        // For null key
+        Hashtable<Integer, Double> m = new Hashtable<>();
+        try {
+            m.getOrDefault(null, -1.0);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+
+    public void test_forEach() {
+        MapDefaultMethodTester.test_forEach(new Hashtable<>());
+
+        // Null pointer exception for empty function
+        Map<Integer, Double> m = new Hashtable<>();
+        try {
+            m.forEach(null);
+        } catch (NullPointerException e) {
+        }
+    }
+
+    public void test_putIfAbsent() {
+        MapDefaultMethodTester.test_putIfAbsent(new Hashtable<>());
+
+        // Null pointer exception for empty function
+        Map<Integer, Double> m = new Hashtable<>();
+        try {
+            m.putIfAbsent(null, 1.0);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+
+        try {
+            m.putIfAbsent(5, null);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+
+    public void test_remove() {
+        MapDefaultMethodTester.test_remove(new Hashtable<>());
+
+        // NPE for null key
+        Map<Integer, Double> m = new Hashtable<>();
+        try {
+            m.remove(null, 2.0);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+
+    public void test_replace$K$V$V() {
+        MapDefaultMethodTester.test_replace$K$V$V(new Hashtable<>());
+
+        // NPE for null key
+        Hashtable<Integer, Double> m = new Hashtable<>();
+        try {
+            m.replace(null, 1.0, 5.0);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+
+        // NPE for null value
+        m.put(1, 5.0);
+        try {
+            m.replace(1, 5.0, null);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+
+    public void test_replace$K$V() {
+        MapDefaultMethodTester.test_replace$K$V(new Hashtable<>(), false/*DoesNotAcceptNullKey*/);
+
+        // NPE for replacing with null key
+        Hashtable<Integer, Double> m = new Hashtable<>();
+        m.put(1, 1.0);
+        try {
+            m.replace(1, null);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+
+        // NPE for null key
+        try {
+            m.replace(null, 5.0);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+
+    public void test_computeIfAbsent() {
+        MapDefaultMethodTester.test_computeIfAbsent(new Hashtable<>(),
+                false/*DoesNotAcceptNullKey*/);
+
+        // NPE for null compute function
+        Map<Integer, Double> m = new Hashtable<>();
+        try {
+            m.computeIfAbsent(10, null);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+
+        // NPE for null key
+        try {
+            m.computeIfAbsent(null, k -> 6.0 * k);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+
+    public void test_computeIfPresent() {
+        MapDefaultMethodTester.test_computeIfPresent(new Hashtable<>(),
+                false/*DoesNotAcceptNullKey*/);
+
+        // NPE for null key
+        Map<Integer, Double> m = new Hashtable<>();
+        try {
+            m.compute(null, (k, v) -> 10.0);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+
+        // NPE for null compute function
+        try {
+            m.compute(1, null);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+
+    public void test_compute() {
+        MapDefaultMethodTester.test_compute(new Hashtable<>(), false/*DoesNotAcceptNullKey*/);
+
+        // NPE for null key
+        Map<Integer, Double> m = new Hashtable<>();
+        try {
+            m.compute(null, (k, v) -> 10.0);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+
+        // NPE for null compute function
+        try {
+            m.compute(1, null);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+
+    public void test_merge() {
+        Map<Integer, Double> m = new Hashtable<>();
+
+        // Checking for an unmapped key
+        m.put(1, 10.0);
+        assertEquals(25.0, m.merge(1, 15.0, (v1, v2) -> v1 + v2));
+        assertEquals(25.0, m.getOrDefault(1, -1.0));
+
+        assertNull(m.merge(1, 2.0, (v1, v2) -> null));
+        assertFalse(m.containsKey(1));
+
+        // NPE for null key
+        try {
+            m.merge(null, 2.0, (v1, v2) -> null);
+            fail();
+        } catch (NullPointerException expected) {
+        }
+    }
+}
