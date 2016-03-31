03dc43e506b2ce4a2cbf5f086cee77a5604ddeb1
Kenny Root <kroot@google.com>
2016-03-30 17:46:56 -0700

    Rewrite CertPathValidatorSpiTest with Mockito
    The existing test isn't testing much, so rewriting in Mockito will make
    it more clear that it is not testing much.
    
    Bug: 27812109
    Change-Id: I6ab24beb4adb1ab57dc9272974e12769d747ce79

--- luni/src/test/java/tests/security/cert/CertPathValidatorSpiTest.java
+++ luni/src/test/java/tests/security/cert/CertPathValidatorSpiTest.java
@@ -22,43 +22,37 @@
 
 package tests.security.cert;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.security.InvalidAlgorithmParameterException;
-import java.security.cert.CertPath;
-import java.security.cert.CertPathParameters;
 import java.security.cert.CertPathValidatorException;
-import java.security.cert.CertPathValidatorResult;
 import java.security.cert.CertPathValidatorSpi;
 
-import org.apache.harmony.security.tests.support.cert.MyCertPathValidatorSpi;
+import org.junit.Test;
+
 /**
  * Tests for <code>CertPathValidatorSpi</code> class constructors and methods.
  *
  */
-public class CertPathValidatorSpiTest extends TestCase {
+public class CertPathValidatorSpiTest {
+    @Test
+    public void testCertPathValidatorSpi_engineValidate() throws Exception {
+        CertPathValidatorSpi cpvs = mock(CertPathValidatorSpi.class);
+        when(cpvs.engineValidate(null, null)).thenReturn(null);
+        assertNull(cpvs.engineValidate(null, null));
+        verify(cpvs, times(1)).engineValidate(null, null);
+    }
 
-    /**
-     * Test for <code>CertPathValidatorSpi</code> constructor Assertion:
-     * constructs CertPathValidatorSpi
-     */
-    public void testCertPathValidatorSpi01() throws CertPathValidatorException,
-            InvalidAlgorithmParameterException {
-        CertPathValidatorSpi certPathValid = new MyCertPathValidatorSpi();
-        CertPathParameters params = null;
-        CertPath certPath = null;
-        CertPathValidatorResult cpvResult = certPathValid.engineValidate(
-                certPath, params);
-        assertNull("Not null CertPathValidatorResult", cpvResult);
-        try {
-            certPathValid.engineValidate(certPath, params);
-            fail("CertPathValidatorException must be thrown");
-        } catch (CertPathValidatorException e) {
-        }
-        try {
-            certPathValid.engineValidate(certPath, params);
-            fail("InvalidAlgorithmParameterException must be thrown");
-        } catch (InvalidAlgorithmParameterException e) {
-        }
+    @Test(expected = CertPathValidatorException.class)
+    public void engineValidate_throws_CertPathValidatorException() throws Exception {
+        CertPathValidatorSpi cpvs = mock(CertPathValidatorSpi.class);
+        doThrow(new CertPathValidatorException()).when(cpvs).engineValidate(null, null);
+        cpvs.engineValidate(null, null);
+    }
     }
 }
