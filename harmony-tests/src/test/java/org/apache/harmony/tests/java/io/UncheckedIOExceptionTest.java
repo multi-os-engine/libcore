2115cc0ca7d60965a361129d8826478ff2d847e4
Yi Kong <yikong@google.com>
2016-04-08 15:09:30 +0100

    Port a number of OpenJDK8 stream methods
    Ports the following methods with tests:
    java.io.BufferedReader#lines
    java.util.BitSet#stream
    java.util.Random#doubles
    java.util.Random#ints
    java.util.Random#longs
    java.util.regex.Pattern#asPredicate
    java.util.regex.Pattern#splitAsStream
    
    Bug: 28108421
    Change-Id: I4813221c69c207ba42ae306d231f3f7c807abbc6

---
+++ harmony-tests/src/test/java/org/apache/harmony/tests/java/io/UncheckedIOExceptionTest.java
@@ -0,0 +1,55 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.harmony.tests.java.io;
+
+import junit.framework.TestCase;
+
+import org.apache.harmony.testframework.serialization.SerializationTest;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+public class UncheckedIOExceptionTest extends TestCase {
+
+  /**
+   * java.lang.UncheckedIOException#UncheckedIOException(java.io.IOException)
+   */
+  public void test_ConstructorLjava_lang_IOException() {
+    IOException ioException = new IOException();
+    UncheckedIOException e = new UncheckedIOException(ioException);
+    assertEquals("java.io.IOException", e.getMessage());
+    assertSame(ioException, e.getCause());
+  }
+
+  /**
+   * java.lang.UncheckedIOException#UncheckedIOException(java.lang.String, java.io.IOException)
+   */
+  public void test_ConstructorLjava_lang_String_IOException() {
+    IOException ioException = new IOException();
+    UncheckedIOException e = new UncheckedIOException("errmsg", ioException);
+    assertEquals("errmsg", e.getMessage());
+    assertSame(ioException, e.getCause());
+  }
+
+  /**
+   * serialization/deserialization.
+   */
+  public void testSerializationSelf() throws Exception {
+    SerializationTest.verifySelf(new UncheckedIOException(new IOException()));
+  }
+}
