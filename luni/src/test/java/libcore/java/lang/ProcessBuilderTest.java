006092f3145b6e769642dfc979d9500d9fc84ee0
Tobias Thierer <tobiast@google.com>
2016-05-06 20:57:21 +0100

    Test for Process in/out/err redirect from/to file/pipe via ProcessBuilder
    The tests run
      - "sh -c cat" to test in/out redirect, and
      - "ls /tmp/missing-file/ProcessBuilderTest" to test error redirect.
    
    The tests use a new helper method checkProcessExecution() which
    has a similar purpose to Support_Exec.execAndCheckOutput() but also
    supports writing the process's input and checking the result code.
    It may be nice to refactor the existing callers of execAndCheckOutput()
    to use checkProcessExecution() instead, but I haven't done that.
    
    Redirect.INHERIT remains untested, mostly because I can't think of a
    good way to test it.
    
    Tested: vogar --mode=host --classpath \
    out/target/common/obj/JAVA_LIBRARIES/core-tests_intermediates/classes.jack \
    libcore/luni/src/test/java/libcore/java/lang/ProcessBuilderTest.java
    
    Bug: 27464570
    Change-Id: Ieb6dd53e363f352df86106016222b1378039fa41

--- luni/src/test/java/libcore/java/lang/ProcessBuilderTest.java
+++ luni/src/test/java/libcore/java/lang/ProcessBuilderTest.java
@@ -16,15 +16,22 @@
 
 package libcore.java.lang;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Writer;
 import java.lang.ProcessBuilder.Redirect;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import libcore.io.Base64;
+import libcore.io.IoUtils;
 import libcore.java.util.AbstractResourceLeakageDetectorTestCase;
 
 import static java.lang.ProcessBuilder.Redirect.INHERIT;
@@ -32,6 +39,7 @@ import static java.lang.ProcessBuilder.Redirect.PIPE;
 import static tests.support.Support_Exec.execAndCheckOutput;
 
 public class ProcessBuilderTest extends AbstractResourceLeakageDetectorTestCase {
+    private static final String TAG = ProcessBuilderTest.class.getSimpleName();
 
     private static String shell() {
         String deviceSh = System.getenv("ANDROID_ROOT") + "/bin/sh";
@@ -52,6 +60,100 @@ public class ProcessBuilderTest extends AbstractResourceLeakageDetectorTestCase 
 
     public void test_redirectErrorStream_false() throws Exception {
         assertRedirectErrorStream(false, "out\n", "err\n");
+    }
+
+    public void testRedirectFile_input() throws Exception {
+        String inputFileContents = "process input for testing\n" + TAG;
+        File file = File.createTempFile(TAG, "in");
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(inputFileContents);
+        }
+        ProcessBuilder pb = new ProcessBuilder(shell(), "-c", "cat").redirectInput(file);
+        checkProcessExecution(pb, /* expectedResultCode */ 0, /* processInput */ "",
+                /* expectedOutput */ inputFileContents, /* expectedError */ "");
+        assertTrue(file.delete());
+    }
+
+    public void testRedirectFile_output() throws Exception {
+        File file = File.createTempFile(TAG, "out");
+        int expectedResultCode = 0;
+        String processInput = TAG + "\narbitrary string for testing!";
+        ProcessBuilder pb = new ProcessBuilder(shell(), "-c", "cat")
+                .redirectOutput(file);
+        checkProcessExecution(pb, expectedResultCode, processInput,
+                /* expectedOutput */ "", /* expectedError */ "");
+
+        String fileContents = new String(IoUtils.readFileAsByteArray(
+                file.getAbsolutePath()));
+        assertEquals(processInput, fileContents);
+        assertTrue(file.delete());
+    }
+
+    public void testRedirectFile_error() throws Exception {
+        File file = File.createTempFile(TAG, "err");
+        String processInput = "";
+        int expectedResultCode = 2; // documented in man ls
+
+        String missingFilePath = "/tmp/missing-file/" + TAG;
+        ProcessBuilder pb = new ProcessBuilder("ls", missingFilePath)
+                .redirectError(file);
+        checkProcessExecution(pb, expectedResultCode, processInput,
+                /* expectedOutput */ "", /* expectedError */ "");
+
+        String fileContents = new String(IoUtils.readFileAsByteArray(file.getAbsolutePath()));
+        // Assuming that the path of the missing file occurs in the ls output is probably safe?
+        assertTrue("Unexpected output: " + fileContents,
+                fileContents.contains(missingFilePath) && !fileContents.equals(missingFilePath));
+    }
+
+    public void testRedirectPipe_inputAndOutput() throws Exception {
+        //checkProcessExecution(pb, expectedResultCode, processInput, expectedOutput, expectedError)
+
+        String testString = "process input and output for testing\n" + TAG;
+        {
+            ProcessBuilder pb = new ProcessBuilder(shell(), "-c", "cat")
+                    .redirectInput(PIPE)
+                    .redirectOutput(PIPE);
+            checkProcessExecution(pb, 0, testString, testString, "");
+        }
+
+        // Check again without specifying PIPE explicitly, since that is the default
+        {
+        ProcessBuilder pb = new ProcessBuilder(shell(), "-c", "cat");
+        checkProcessExecution(pb, 0, testString, testString, "");
+        }
+
+        // Because the above test is symmetric regarding input vs. output, test
+        // another case where input and output are different.
+        {
+            ProcessBuilder pb = new ProcessBuilder("echo", testString);
+            checkProcessExecution(pb, 0, "", testString + "\n", "");
+        }
+    }
+
+    public void testRedirectPipe_error() throws Exception {
+        String missingFilePath = "/tmp/missing-file/" + TAG;
+
+        // Can't use checkProcessExecution because we don't want to rely on an exact error content
+        Process process = new ProcessBuilder("ls", missingFilePath)
+                .redirectError(Redirect.PIPE).start();
+        process.getOutputStream().close(); // no process input
+        assertEquals(2, process.waitFor());
+        assertEquals("", new String(readFully(process.getInputStream()))); // no process output
+        String errorString = new String(readFully(process.getErrorStream()));
+        // Assuming that the path of the missing file occurs in the ls output is probably safe?
+        assertTrue("Unexpected output: " + errorString,
+                errorString.contains(missingFilePath) && !errorString.equals(missingFilePath));
+    }
+
+    /**
+     * Writes Base64 encoded bytes, followed by the raw bytes, to the given {@code outputStream}.
+     */
+    private static void writeBase64AndRawBytesTo(byte[] bytes, OutputStream outputStream)
+            throws IOException {
+        String base64OfBytes = Base64.encode(bytes);
+        outputStream.write(base64OfBytes.getBytes()); // platform default charset
+        outputStream.write(bytes); // trailing garbage
     }
 
     public void testEnvironment() throws Exception {
@@ -186,6 +288,10 @@ public class ProcessBuilderTest extends AbstractResourceLeakageDetectorTestCase 
         assertEquals(redirect.toString(), expectedType, redirect.type());
     }
 
+    public void testRedirect_defaultsToPipe() {
+        checkSetAndGet(PIPE, PIPE, PIPE, new ProcessBuilder());
+    }
+
     public void testRedirect_setAndGet() {
         File file = new File("/tmp/fake-file-for/java.lang.ProcessBuilderTest");
         checkSetAndGet(Redirect.from(file), PIPE, PIPE, new ProcessBuilder().redirectInput(file));
@@ -197,6 +303,27 @@ public class ProcessBuilderTest extends AbstractResourceLeakageDetectorTestCase 
                         .redirectOutput(INHERIT)
                         .redirectError(file)
                         .redirectInput(file));
+    }
+
+    /**
+     * Starts the specified process, writes the specified input to it and waits for the process
+     * to finish; then, then checks that the result code and output / error are expected.
+     *
+     * <p>This method assumes that the process consumes and produces character data encoded with
+     * the platform default charset.
+     */
+    private static void checkProcessExecution(ProcessBuilder pb, int expectedResultCode,
+            String processInput,
+            String expectedOutput, String expectedError) throws Exception {
+        Process process = pb.start();
+        Future<byte[]> processOutput = asyncRead(process.getInputStream());
+        Future<byte[]> processError = asyncRead(process.getErrorStream());
+        try (OutputStream outputStream = process.getOutputStream()) {
+            outputStream.write(processInput.getBytes()); // platform default charset
+        }
+        assertEquals(expectedResultCode, process.waitFor());
+        assertEquals(expectedOutput, new String(processOutput.get()));
+        assertEquals(expectedError, new String(processError.get()));
     }
 
     private static void checkSetAndGet(Redirect in, Redirect out, Redirect err, ProcessBuilder pb) {
@@ -214,6 +341,35 @@ public class ProcessBuilderTest extends AbstractResourceLeakageDetectorTestCase 
         assertEquals(a, b);
         assertEquals(b, a);
         assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    private static void assertArrayEquals(byte[] expected, byte[] actual) {
+        String msg = "Expected " + Arrays.toString(expected) + ", got " + new String(actual);
+        assertTrue(msg, Arrays.equals(expected, actual));
+    }
+
+    static byte[] readFully(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] data = new byte[1024];
+        int numRead;
+        while ((numRead = inputStream.read(data)) >= 0) {
+            outputStream.write(data, 0, numRead);
+        }
+        return outputStream.toByteArray();
+    }
+
+    /**
+     * Reads the entire specified {@code inputStream} asynchronously.
+     */
+    static FutureTask<byte[]> asyncRead(final InputStream inputStream) {
+        final FutureTask<byte[]> result = new FutureTask<>(() -> readFully(inputStream));
+        new Thread("read asynchronously from " + inputStream) {
+            @Override
+            public void run() {
+                result.run();
+            }
+        }.start();
+        return result;
     }
 
 }
