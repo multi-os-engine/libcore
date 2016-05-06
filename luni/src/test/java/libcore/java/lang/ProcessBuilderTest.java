4dfd34122e4aa73d52dd2dfe293dffcd720c405c
Tobias Thierer <tobiast@google.com>
2016-05-06 20:57:21 +0100

    Test for Process in/out/err redirect from/to file/pipe via ProcessBuilder
    Both the file and Redirect.PIPE redirection tests work by running
    base64 -d on input that contains Base64 encoded known bytes followed
    by the same (un-encoded) bytes as trailing garbage. This produces
    partially known output and error, which can be checked.
    
    Redirect.INHERIT remains untested, mostly because I can't think of a
    good way to test it.
    
    Tested: vogar --mode=host --classpath \
    out/target/common/obj/JAVA_LIBRARIES/core-tests_intermediates/classes.jack \
    libcore/luni/src/test/java/libcore/java/lang/ProcessBuilderTest.java
    
    Bug: 27464570
    Change-Id: Ieb6dd53e363f352df86106016222b1378039fa41

--- luni/src/test/java/libcore/java/lang/ProcessBuilderTest.java
+++ luni/src/test/java/libcore/java/lang/ProcessBuilderTest.java
@@ -16,16 +16,27 @@
 
 package libcore.java.lang;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import libcore.io.Base64;
+import libcore.io.IoUtils;
 import libcore.java.util.AbstractResourceLeakageDetectorTestCase;
+
 import static tests.support.Support_Exec.execAndCheckOutput;
 
 public class ProcessBuilderTest extends AbstractResourceLeakageDetectorTestCase {
+    private static final String TAG = ProcessBuilderTest.class.getSimpleName();
 
     private static String shell() {
         String deviceSh = System.getenv("ANDROID_ROOT") + "/bin/sh";
@@ -46,6 +57,93 @@ public class ProcessBuilderTest extends AbstractResourceLeakageDetectorTestCase 
 
     public void test_redirectErrorStream_false() throws Exception {
         assertRedirectErrorStream(false, "out\n", "err\n");
+    }
+
+    /**
+     * Tests input, output and error redirection from/to files.
+     * This checks that "base64 -d" on input that starts validly, but has
+     * trailing garbage, produces expected output and errors.
+     */
+    public void testRedirectAll_file() throws IOException {
+        File inFile = File.createTempFile(TAG, "in");
+        byte[] rawBytes = new byte[100000];
+        new Random(0xc0ffee).nextBytes(rawBytes); // arbitrary but deterministic bytes
+        try (OutputStream outputStream = new FileOutputStream(inFile)) {
+            writeBase64AndRawBytesTo(rawBytes, outputStream);
+        }
+        File outFile = File.createTempFile(TAG, "out");
+        File errFile = File.createTempFile(TAG, "err");
+        try {
+            ProcessBuilder pb = new ProcessBuilder()
+                    .command("base64", "-d")
+                    .redirectInput(inFile)
+                    .redirectOutput(outFile)
+                    .redirectError(errFile);
+            Process process = pb.start();
+            int result = process.waitFor();
+            assertEquals(pb.command() + " exit value", 1, result);
+            byte[] out = IoUtils.readFileAsByteArray(outFile.getAbsolutePath());
+            byte[] err = IoUtils.readFileAsByteArray(errFile.getAbsolutePath());
+
+            byte[] expectedErrSuffix = "invalid input\n".getBytes(); // platform default charset
+            assertPrefix("out should start with the raw bytes", rawBytes, out);
+            assertSuffix("err should end with error message", expectedErrSuffix, err);
+        } catch (InterruptedException e) {
+            fail("Unexpected interruption: " + e.getMessage());
+        } finally {
+            for (File file : Arrays.asList(inFile, outFile, errFile)) {
+                boolean ignoredSuccess = file.delete();
+            }
+        }
+    }
+
+    /**
+     * Tests input, output and error redirection from/to PIPE.
+     * This is similar to {@link #testRedirectAll_file()} however in this
+     * case we need to start the process before we can write to its input.
+     */
+    public void testRedirectAll_pipe() throws IOException {
+        ProcessBuilder pb = new ProcessBuilder()
+                .command("base64", "-d")
+                .redirectInput(Redirect.PIPE)
+                .redirectOutput(Redirect.PIPE)
+                .redirectError(Redirect.PIPE);
+        Process process = pb.start();
+        // start reading so that the writer doesn't block
+        FutureTask<byte[]> futureOut = asyncRead(process.getInputStream());
+        FutureTask<byte[]> futureErr = asyncRead(process.getErrorStream());
+        byte[] rawBytes = new byte[100000];
+        new Random(0xc0ffee).nextBytes(rawBytes); // arbitrary but deterministic bytes
+        try (OutputStream processStdIn = process.getOutputStream()) {
+            try {
+                writeBase64AndRawBytesTo(rawBytes, processStdIn);
+            } catch (IOException e) {
+                // ignore the pipe breaking once the reading process encounters the garbage
+            }
+        }
+        try {
+            int result = process.waitFor();
+            assertEquals(pb.command() + " exit value", 1, result);
+            byte[] out = futureOut.get();
+            byte[] err = futureErr.get();
+            byte[] expectedErrSuffix = "invalid input\n".getBytes(); // platform default charset
+            assertPrefix("out should start with the raw bytes", rawBytes, out);
+            assertSuffix("err should end with error message", expectedErrSuffix, err);
+        } catch (InterruptedException e) {
+            fail("Unexpected interruption: " + e.getMessage());
+        } catch (ExecutionException e) {
+            fail("Unexpected ExecutionException: " + e.getCause().getMessage());
+        }
+    }
+
+    /**
+     * Writes Base64 encoded bytes, followed by the raw bytes, to the given {@code outputStream}.
+     */
+    private static void writeBase64AndRawBytesTo(byte[] bytes, OutputStream outputStream)
+            throws IOException {
+        String base64OfBytes = Base64.encode(bytes);
+        outputStream.write(base64OfBytes.getBytes()); // platform default charset
+        outputStream.write(bytes); // trailing garbage
     }
 
     public void testEnvironment() throws Exception {
@@ -104,4 +202,41 @@ public class ProcessBuilderTest extends AbstractResourceLeakageDetectorTestCase 
         }
         assertEquals(before, environment);
     }
+
+    private static void assertPrefix(String message, byte[] expectedPrefix, byte[] data) {
+        byte[] dataPrefix = Arrays.copyOfRange(
+                data, 0, Math.min(expectedPrefix.length, data.length));
+        String comparison = Arrays.toString(expectedPrefix) + " vs. " + Arrays.toString(dataPrefix);
+        assertTrue(message + ": " + comparison, Arrays.equals(expectedPrefix, dataPrefix));
+    }
+
+    private static void assertSuffix(String message, byte[] expectedSuffix, byte[] data) {
+        byte[] dataSuffix = Arrays.copyOfRange(
+                data, Math.max(0, data.length - expectedSuffix.length), data.length);
+        String comparison = Arrays.toString(expectedSuffix) + " vs. " + Arrays.toString(dataSuffix);
+        assertTrue(message + ": " + comparison, Arrays.equals(expectedSuffix, dataSuffix));
+    }
+
+    /**
+     * Reads the entire specified {@code inputStream} asynchronously.
+     */
+    static FutureTask<byte[]> asyncRead(final InputStream inputStream) {
+        final FutureTask<byte[]> result = new FutureTask<>(() -> {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            byte[] data = new byte[1024];
+            int numRead;
+            while ((numRead = inputStream.read(data)) >= 0) {
+                outputStream.write(data, 0, numRead);
+            }
+            return outputStream.toByteArray();
+        });
+        new Thread("read asynchronously from " + inputStream) {
+            @Override
+            public void run() {
+                result.run();
+            }
+        }.start();
+        return result;
+    }
+
 }
