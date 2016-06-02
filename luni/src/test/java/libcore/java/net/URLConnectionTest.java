4896262314c6fd814c42e5f5a729877b8cf51e6c
Tobias Thierer <tobiast@google.com>
2016-06-02 16:44:46 +0100

    prepare for okhttp upgrade: Fix URLConnectionTest.testConnectTimeouts
    I fixed this test by dropping a dependency on Mockito which no
    longer works with okhttp 2.7.5 for some reason. The new version
    of the test should also work with the old code, so I'm changing
    it ahead of the planned okhttp upgrade.
    
    More detail:
    In http://r.android.com/169087 this code changed to use spy() fake.
    With okhttp Mockito 2.7.5, this test fails in mockito's
    ConditionalStackTraceFilter.<init> because the
    ClassPathLoader.getStackTraceCleanerProvider() returns null.
    I suspect it's due to a bug in the order of static initializers
    being run (the method has a TODO from 2013 to throw a  better
    exception). I haven't figured out why it appears to happen only
    after upgrading okhttp.
    
    Since the dependency on Mockito isn't actually required for this
    fake, I've implemented our own fake logic.
    
    I've also tweaked the comment in URLConnectionTest which previously
    claimed that the socketSpy was a mock (as opposed to a fake) and
    that it 'wraps the actual socket', both of which I believe were
    incorrect.
    
    Tested:
    make cts && cts-tradefed run cts -c libcore.java.net.URLConnectionTest
    
    Bug: 28935102
    Change-Id: I792eef20fd9744b9e9b7a5b4b32ddf078af3c433

--- luni/src/test/java/libcore/java/net/URLConnectionTest.java
+++ luni/src/test/java/libcore/java/net/URLConnectionTest.java
@@ -91,10 +91,6 @@ import static com.google.mockwebserver.SocketPolicy.DISCONNECT_AT_START;
 import static com.google.mockwebserver.SocketPolicy.FAIL_HANDSHAKE;
 import static com.google.mockwebserver.SocketPolicy.SHUTDOWN_INPUT_AT_END;
 import static com.google.mockwebserver.SocketPolicy.SHUTDOWN_OUTPUT_AT_END;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.spy;
 
 public final class URLConnectionTest extends AbstractResourceLeakageDetectorTestCase {
 
@@ -1970,22 +1966,23 @@ public final class URLConnectionTest extends AbstractResourceLeakageDetectorTest
                 @Override
                 protected Socket configureSocket(Socket socket) throws IOException {
                     final int attemptNumber = socketCreationCount[0]++;
-                    Answer socketConnectAnswer = new Answer() {
-                        @Override public Object answer(InvocationOnMock invocation)
-                                throws Throwable {
-                            int timeoutArg = (int) invocation.getArguments()[1];
-                            socketConnectTimeouts[attemptNumber] = timeoutArg;
-                            throw new SocketTimeoutException(
-                                "Simulated timeout after " + timeoutArg);
+                    // Create a fake Socket that intercepts connect() calls.
+                    Socket result = new Socket() {
+                        @Override
+                        public void connect(SocketAddress endpoint, int timeout)
+                                throws IOException {
+                            socketConnectTimeouts[attemptNumber] = timeout;
+                            throw new SocketTimeoutException("Simulated timeout after " + timeout);
+                        }
+
+                        @Override
+                        public void connect(SocketAddress endpoint) throws IOException {
+                            // This is the same thing the super class does as of 2016-06,
+                            // we're just making it explicit here.
+                            connect(endpoint, 0);
                         }
                     };
-
-                    Socket socketSpy = spy(socket);
-                    // Create a partial mock that wraps the actual socket and intercepts the
-                    // connect(SocketAddress, int) method.
-                    doAnswer(socketConnectAnswer)
-                        .when(socketSpy).connect(any(SocketAddress.class), anyInt());
-                    return socketSpy;
+                    return result;
                 }
             });
 
